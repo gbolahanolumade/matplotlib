a89a0c2a623d26bf83fbe5a73b8783893c0e1c36
gbolahanolumade <olumadegbolahan@gmail.com>
2020-12-15 13:13:48 +0100

    Date Plot

--- Matplot.py
+++ Matplot.py
@@ -34,3 +34,59 @@ plt.plot(t,y1,'b--',t,y2,'g',t,y3,'r-.')
 
 math.sin(t)
 plt.plot([1,2,4,2,1,0,1,2,1,4],linewidth=2.0)
+
+t = np.arange(0,5,0.1)
+y1 = np.sin(2*np.pi*t)
+y2 = np.sin(2*np.pi*t)
+plt.subplot(211)
+plt.plot(t,y1,'b-.')
+plt.subplot(212)
+plt.plot(t,y2,'r--')
+
+
+plt.axis([0,5,0,20])
+plt.title('My first plot')
+plt.xlabel('Counting')
+plt.ylabel('Square Values')
+plt.plot([1,2,3,4],[1,4,9,16], 'ro')
+
+plt.axis([0,5,0,20])
+plt.title('My first plot', fontsize=20, fontname='Times New Roman')
+plt.xlabel('Counting', color='gray')
+plt.ylabel('Square', color='gray')
+plt.plot([1,2,3,4],[1,4,9,16], 'ro')
+
+
+plt.axis([0,5,0,20])
+plt.title('My first plot', fontsize=20, fontname='Times New Roman')
+plt.xlabel('Counting', color='gray')
+plt.ylabel('Square', color='gray')
+plt.text(1,1.5, 'First')
+plt.text(2,4.5, 'Second')
+plt.text(3,9.5, 'Third')
+plt.text(4,16.5, 'Third')
+plt.plot([1,2,3,4],[1,4,9,16], 'ro')
+
+import datetime
+events = [datetime.date(2015,1,23),datetime.date(2015,1,28),datetime.
+date(2015,2,3),datetime.date(2015,2,21),datetime.date(2015,3,15),datetime.
+date(2015,3,24),datetime.date(2015,4,8),datetime.date(2015,4,24)]
+readings = [12,22,25,20,18,15,17,14]
+plt.plot(events,readings)
+
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+months = mdates.MonthLocator()
+days = mdates.DayLocator()
+timeFmt = mdates.DateFormatter('%Y-%m')
+events = [datetime.date(2015,1,23),datetime.date(2015,1,28),datetime.
+date(2015,2,3),datetime.date(2015,2,21),datetime.date(2015,3,15),datetime.
+date(2015,3,24),datetime.date(2015,4,8),datetime.date(2015,4,24)]
+readings = [12,22,25,20,18,15,17,14]
+fig, ax = plt.subplots()
+plt.plot(events,readings)
+ax.xaxis.set_major_locator(months)
+ax.xaxis.set_major_formatter(timeFmt)
+ax.xaxis.set_minor_locator(days)
